fcbdcdcff2d36ddc8b3c3a588fb2d8c4dd3fb64b
mdmuche <abulamartins@gmail.com>
2025-03-11 16:45:42 +0100

    initial

---
+++ class2.py
@@ -0,0 +1,10 @@
+print("hello, welcome to python")
+name = input("enter your name: ")
+print("welcome " + name)
+feeling = input("how do you feel: ")
+if feeling == "sad":
+  print("i am feeling " + feeling)
+elif feeling == "happy":
+  print("i am feeling " + feeling)
+else:
+  print(feeling + " not valid")
