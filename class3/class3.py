3945bd70f7e384ccfc130c2502f9f7b4a1dd8f77
mdmuche <abulamartins@gmail.com>
2025-03-28 08:11:44 -0700

    loops

---
+++ class3/class3.py
@@ -0,0 +1,20 @@
+## loops
+# for loop
+# for loop repeats things based on number of items in a structure
+# x = range(5)
+# print(x)
+
+# age = range(5)
+
+# for i in age:
+#     print(i)
+
+
+# while loop
+# while repeats things based on a condition and the condition must be true
+
+name = "mercy"
+
+while name == "mercy":
+    print("while loop...")
+    name = "obada"
